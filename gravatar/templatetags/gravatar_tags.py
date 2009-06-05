04f107df244f3439f4a0a6f28f527450131c7b0c
Marcus Fredriksson <drmegahertz@gmail.com>
2009-06-05 15:12:38 +0200

    Initial import of the highly experimental cache code
    This is no way near done, so don't blame me if it doesn't work on
    your first try.

--- gravatar/templatetags/gravatar_tags.py
+++ gravatar/templatetags/gravatar_tags.py
@@ -2,14 +2,26 @@ from django import template
 from django.conf import settings
 from hashlib import md5
 from urllib import urlencode
+import os.path
+import urllib
+import urlparse
+import threading
 
 GRAVATAR_URL_PREFIX = getattr(settings, 'GRAVATAR_URL_PREFIX', 'http://www.gravatar.com/avatar/')
 GRAVATAR_IMAGE_EXT = getattr(settings, 'GRAVATAR_IMAGE_EXT', '')
 GRAVATAR_DEFAULT_IMAGE = getattr(settings, 'GRAVATAR_DEFAULT_IMAGE', '')
 GRAVATAR_RATING = getattr(settings, 'GRAVATAR_RATING', '')
 GRAVATAR_SIZE = getattr(settings, 'GRAVATAR_SIZE', '')
+GRAVATAR_CACHE_DIR = getattr(settings, 'GRAVATAR_CACHE_DIR', 'img/gravatar/')
+GRAVATAR_MEDIA_PREFIX = getattr(settings, 'GRAVATAR_MEDIA_PREFIX', '/media/img/gravatar/')
 
 register = template.Library()
+
+def download_gravatar(url, dest):
+    try:
+        urllib.urlretrieve(url, dest)
+    except IOError:
+        pass
 
 @register.simple_tag
 def get_gravatar_for_email(email, size = None, rating = None):
@@ -40,4 +52,20 @@ def get_gravatar_for_email(email, size = None, rating = None):
     if parameters:
         gravatar_url += '?' + urlencode(parameters, doseq = True)
 
-    return gravatar_url
+    gravatar_local = os.path.join(settings.MEDIA_ROOT, GRAVATAR_CACHE_DIR, '%s.jpg' % gravatar_id)
+
+    if not os.path.isfile(gravatar_local):
+        t = threading.Thread(
+            target = download_gravatar,
+            args = (
+                gravatar_url, 
+                gravatar_local
+            )
+        )
+
+        t.setDaemon(True)
+        t.start()
+
+        return gravatar_url
+
+    return urlparse.urljoin(GRAVATAR_MEDIA_PREFIX, '%s.jpg' % gravatar_id)
