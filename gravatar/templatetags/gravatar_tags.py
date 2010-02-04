8007ca105115e5d23f5cef06b873c666738591d0
ram <ramusus@gmail.com>
2010-02-05 00:41:29 +0300

    added function for importing

--- gravatar/templatetags/gravatar_tags.py
+++ gravatar/templatetags/gravatar_tags.py
@@ -7,13 +7,7 @@ GRAVATAR_URL_PREFIX = getattr(settings, 'GRAVATAR_URL_PREFIX', 'http://www.grava
 GRAVATAR_IMAGE_EXT = getattr(settings, 'GRAVATAR_IMAGE_EXT', '')
 GRAVATAR_RATING = getattr(settings, 'GRAVATAR_RATING', '')
 GRAVATAR_SIZE = getattr(settings, 'GRAVATAR_SIZE', '')
-
-if hasattr(settings, "GRAVATAR_DEFAULT_IMAGE"):
-    from django.contrib.sites.models import Site
-    GRAVATAR_DEFAULT_IMAGE = 'http://%s%s' % (Site.objects.get_current().domain,
-                                              getattr(settings, "GRAVATAR_DEFAULT_IMAGE"))
-else:
-    GRAVATAR_DEFAULT_IMAGE = ''
+GRAVATAR_DEFAULT_IMAGE = _get_default_image_url(getattr(settings, "GRAVATAR_DEFAULT_IMAGE")) if hasattr(settings, "GRAVATAR_DEFAULT_IMAGE") else ''
 
 register = template.Library()
 
@@ -30,15 +24,25 @@ def get_gravatar_for_email(email, size = None, rating = None):
 
         {% get_gravatar_for_email foobar@example.com 48 r %}
     """
+    return get_gravatar(email, size, rating)
+
+def get_gravatar(email, size = None, rating = None, default = None):
+    '''
+    Method returns URL string for gravatar image.
+    Not only for template tag, also can be used as simple function
+    '''
     gravatar_id = md5_constructor(email.lower()).hexdigest()
     gravatar_url = GRAVATAR_URL_PREFIX + gravatar_id + GRAVATAR_IMAGE_EXT
+
+    if default:
+        default = _get_default_image_url(default)
 
     # Build a list of tuples with Gravatar parameters that we want to use.
     #
     # This makes sure that no empty parameter will get appended to our final
     # URL, which makes it a little bit shorter and cleaner.
     parameters = [p for p in (
-        ('d', GRAVATAR_DEFAULT_IMAGE),
+        ('d', default or GRAVATAR_DEFAULT_IMAGE),
         ('s', size or GRAVATAR_SIZE),
         ('r', rating or GRAVATAR_RATING),
     ) if p[1]]
@@ -47,3 +51,7 @@ def get_gravatar_for_email(email, size = None, rating = None):
         gravatar_url += '?' + urlencode(parameters, doseq = True)
 
     return gravatar_url
+
+def _get_default_image_url(relative_path):
+    from django.contrib.sites.models import Site
+    return 'http://%s%s' % (Site.objects.get_current().domain, relative_path)
